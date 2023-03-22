e0e6d49b83bbc594e2d0822f4b22cd142a10d1e1
karl <step.sv@mail.ru>
2023-03-22 14:24:54 +0700

    adding to git

---
+++ src/main/java/org/example/app/services/BookRepository.java
@@ -0,0 +1,66 @@
+package org.example.app.services;
+
+import org.apache.log4j.Logger;
+import org.example.web.dto.Book;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class BookRepository implements ProjectRepository<Book>, ApplicationContextAware {
+
+    private final Logger logger = Logger.getLogger(BookRepository.class);
+    private final List<Book> repo = new ArrayList<>();
+    private ApplicationContext context;
+
+    @Override
+    public List<Book> retreiveAll() {
+        return new ArrayList<>(repo);
+    }
+
+    @Override
+    public void store(Book book) {
+        book.setId(context.getBean(IdProvider.class).provideId(book));
+        logger.info("store new book: " + book);
+        repo.add(book);
+    }
+
+    @Override
+    public boolean removeItemById(String bookIdToRemove) {
+        for (Book book : retreiveAll()) {
+            if (book.getId().equals(bookIdToRemove)) {
+                logger.info("remove book completed: " + book);
+                return repo.remove(book);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void removeByRegex(String queryRegex) {
+        for (Book book: retreiveAll()) {
+            if (book.getAuthor().equals(queryRegex)
+                    || book.getTitle().equals(queryRegex)
+                    || Objects.equals(book.getSize(), Integer.valueOf(queryRegex)))
+                repo.remove(book);
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
+    private void defaultInit() {
+        logger.info("default INIT in book repo bean");
+    }
+
+    private void defaultDestroy() {
+        logger.info("default DESTROY in book repo bean");
+    }
+}
---
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -0,0 +1,63 @@
+package org.example.web.controllers;
+
+import org.apache.log4j.Logger;
+import org.example.app.services.BookService;
+import org.example.web.dto.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/books")
+@Scope("singleton")
+public class BookShelfController {
+
+    private Logger logger = Logger.getLogger(BookShelfController.class);
+    private BookService bookService;
+
+    @Autowired
+    public BookShelfController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/shelf")
+    public String books(Model model) {
+        logger.info(this.toString());
+        model.addAttribute("book", new Book());
+        model.addAttribute("bookList", bookService.getAllBooks());
+        return "book_shelf";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        if (!book.getAuthor().isEmpty()
+                || !book.getTitle().isEmpty()
+                || (book.getSize() != null && book.getSize() > 0)) {
+            bookService.saveBook(book);
+        }
+        logger.info("current repository size: " + bookService.getAllBooks().size());
+        return "redirect:/books/shelf";
+    }
+
+    @PostMapping("/remove")
+    public String removeBook(@RequestParam(value = "bookIdToRemove") String bookIdToRemove) {
+//        if (bookService.removeBookById(bookIdToRemove)) {
+//            return "redirect:/books/shelf";
+//        } else {
+//            return "book_shelf";
+//        }
+        bookService.removeBookById(bookIdToRemove);
+        return "redirect:/books/shelf";
+    }
+
+    @PostMapping("/removeByRegex")
+    public String removeByRegex(@RequestParam(value = "queryRegex") String queryRegex) {
+        bookService.removeByRegex(queryRegex);
+        return "redirect:/books/shelf";
+    }
+}

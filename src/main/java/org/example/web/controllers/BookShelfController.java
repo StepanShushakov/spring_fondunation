d11508d69cc58f1f514519906a1ba4f355e11795
karl <step.sv@mail.ru>
2023-03-27 11:23:01 +0700

    module2-all-lessons-completed

--- src/main/java/org/example/web/controllers/BookShelfController.java
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -3,14 +3,20 @@ package org.example.web.controllers;
 import org.apache.log4j.Logger;
 import org.example.app.services.BookService;
 import org.example.web.dto.Book;
+import org.example.web.dto.BookIdToRemove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.*;
 
 @Controller
 @RequestMapping(value = "/books")
@@ -29,12 +35,19 @@ public class BookShelfController {
     public String books(Model model) {
         logger.info(this.toString());
         model.addAttribute("book", new Book());
+        model.addAttribute("bookIdToRemove", new BookIdToRemove());
         model.addAttribute("bookList", bookService.getAllBooks());
         return "book_shelf";
     }
 
     @PostMapping("/save")
-    public String saveBook(Book book) {
+    public String saveBook(@Valid Book book, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("book", book);
+            model.addAttribute("bookIdToRemove", new BookIdToRemove());
+            model.addAttribute("bookList", bookService.getAllBooks());
+            return "book_shelf";
+        }
         if (!book.getAuthor().isEmpty()
                 || !book.getTitle().isEmpty()
                 || (book.getSize() != null && book.getSize() > 0)) {
@@ -45,19 +58,43 @@ public class BookShelfController {
     }
 
     @PostMapping("/remove")
-    public String removeBook(@RequestParam(value = "bookIdToRemove") String bookIdToRemove) {
-//        if (bookService.removeBookById(bookIdToRemove)) {
-//            return "redirect:/books/shelf";
-//        } else {
-//            return "book_shelf";
-//        }
-        bookService.removeBookById(bookIdToRemove);
-        return "redirect:/books/shelf";
+    public String removeBook(@Valid BookIdToRemove bookIdToRemove, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("book", new Book());
+            model.addAttribute("bookList", bookService.getAllBooks());
+            return "book_shelf";
+        } else {
+            bookService.removeBookById(bookIdToRemove.getId());
+            return "redirect:/books/shelf";
+        }
     }
 
     @PostMapping("/removeByRegex")
     public String removeByRegex(@RequestParam(value = "queryRegex") String queryRegex) {
         bookService.removeByRegex(queryRegex);
+        return "redirect:/books/shelf";
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file")MultipartFile file) throws Exception{
+        String name = file.getOriginalFilename();
+        byte[] bytes = file.getBytes();
+
+        //create dir
+        String rootPath = System.getProperty("catalina.home");
+        File dir = new File(rootPath + File.separator + "external_uploads");
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        //create file
+        File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+        stream.write(bytes);
+        stream.close();
+
+        logger.info("new file saver at: " + serverFile.getAbsolutePath());
+
         return "redirect:/books/shelf";
     }
 }
